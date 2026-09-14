c7c60294b7f271b5cbd9c6960c7f94aaa8add5b7
msafiriglobalmedia-ctrl <msafiriglobalmedia@gmail.com>
2026-09-14 20:44:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -906,6 +906,12 @@ def register(data: RegisterRequest):
             status_code=400,
             detail="Password must be at least 6 characters",
         )
+        
+        if len(password.encode("utf-8")) > 1024:
+    raise HTTPException(
+        status_code=400,
+        detail="Password is too long",
+    )
 
     with get_conn() as conn:
 
